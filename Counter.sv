c741b20cb3f646af030366277d4acaa02e7e7c53
DelishusCake <redkast@gmail.com>
2016-11-30 14:19:09 -0700

    Fixed indentation

--- Counter.sv
+++ Counter.sv
@@ -6,15 +6,15 @@
 module Counter #(
     parameter WORD_LENGTH = 16)
 (
-	input logic clock_i,
-	input logic enable_i,
-	output logic [WORD_LENGTH-1:0] value_o
+    input logic clock_i,
+    input logic enable_i,
+    output logic [WORD_LENGTH-1:0] value_o
 );
-	always_ff @(posedge clock_i) begin
-		if (enable_i) begin
-			value_o <= value_o + 1;
-		end else begin
-			value_o <= 0;
-		end
-	end
+    always_ff @(posedge clock_i) begin
+        if (enable_i) begin
+            value_o <= value_o + 1;
+        end else begin
+            value_o <= 0;
+        end
+    end
 endmodule
